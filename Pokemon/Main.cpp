e76b899ff28af04dee83549b9adf2606eb22463e
utrachet <utrachet@gaming.tech>
2022-11-25 10:49:37 +0100

    merge

--- Pokemon/Main.cpp
+++ Pokemon/Main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Game.h"
 #include "Groudon.h"
 #include "Config.h"
@@ -9,23 +8,6 @@ int main(void)
 
     sf::Texture texture;
     if (!texture.loadFromFile(GROUDON_TEXTURE_PATH))
-=======
-
-
-#include <iostream>
-#include <string>
-#include <SFML/Graphics.hpp>
-
-#include "mainCharacter.h"
-
-using namespace std;
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
-    
-    while (window.isOpen())
->>>>>>> 10f93526d42f58382ee07724ae041038f4ef16f2
     {
         // error...
     }
@@ -42,15 +24,9 @@ int main()
             groudon.nextAnimation();
             count = 0;
         }
-<<<<<<< HEAD
         count++;
         g.drawEntity(groudon);
         g.display();
-=======
-        window.clear();
-        window.draw(mainCharSprite);
-        window.display();
->>>>>>> 10f93526d42f58382ee07724ae041038f4ef16f2
     }
 
     return 0;
