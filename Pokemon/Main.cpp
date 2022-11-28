99f14d64e0ed8370737f6784ec7cc96239651a1a
utrachet <utrachet@gaming.tech>
2022-11-28 16:51:02 +0100

    conflict

--- Pokemon/Main.cpp
+++ Pokemon/Main.cpp
@@ -6,7 +6,7 @@
 int main(void)
 {
     Game g;
-    
+
     int count = 0;
     int e = 0;
     while (g.isOpen())
@@ -42,42 +42,38 @@ int main(void)
             g.displayQuitButton();
 
             g.display();
-         default:
+        default:
             break;
         }
-=======
-        g.clear();
-        g.handleEvents();
-
-        //MainMenu Displaying
-        g.drawBackground();
-        g.drawPokeball();
-        g.drawTitle();
-        g.drawEnterText();
-#include "mainCharacter.h"
-#include "Map.h"
-
-using namespace std;
-
-
-        g.display();
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
-    mainCharacter mainCharMap(&window);
-    
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        mainCharMap.InitChar();
-        window.display();
     }
-
-    return 0;
 }
+
+
+
+//#include "mainCharacter.h"
+//#include "Map.h"
+//
+//using namespace std;
+//
+//
+//        g.display();
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+//    mainCharacter mainCharMap(&window);
+//    
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//        window.clear();
+//        mainCharMap.InitChar();
+//        window.display();
+//    }
+//
+//    return 0;
+//}
